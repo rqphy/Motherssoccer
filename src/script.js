59eeda685796ae4a515be37b3b480efb74db9353
rqphy <raphael.ferreira@hetic.net>
2021-09-26 12:20:18 +0200

    cleaning

--- src/script.js
+++ src/script.js
@@ -101,7 +101,6 @@ document.addEventListener('mouseup', (_event) =>
     {
         const bodyBall = objectsToUpdate.find(obj => obj.mesh.uuid === currentIntersect[0].object.uuid)
         const windowHeight = window.innerHeight > 1200 ? window.innerHeight : 1200
-        console.log(windowHeight, window.innerHeight);
         bodyBall.body.applyLocalForce(
             new CANNON.Vec3((- currentMouse.x - mouse.x) * window.innerWidth * 1.8 , (- currentMouse.y - mouse.y) * windowHeight, -1000),
             new CANNON.Vec3(0, 0, 0)
